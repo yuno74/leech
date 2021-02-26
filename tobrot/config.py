a96dced2adc2068ff4ba3b88fc06b7a766670d57
yuno74 <65702879+yuno74@users.noreply.github.com>
2021-02-26 17:18:01 +0800

    Add files via upload

---
+++ tobrot/config.py
@@ -0,0 +1,15 @@
+from tobrot.sample_config import Config
+#Fill your all data, read readme for reference
+
+class Config(Config):
+    TG_BOT_TOKEN= "1436981788:AAHXzHTgVyUczR5hcx27g1p_uarJYXlCyZ4"
+    APP_ID = 2743279
+    API_HASH = "5eb0b49fbc3a6355feffd2f2bee2187f"
+    OWNER_ID = 1490418027
+    AUTH_CHANNEL = [-1001219689356, -1001493807004, -1001363200135, -1001272467137, -1001310793962]
+    INDEX_LINK = "https://arata74.oreooo.workers.dev/0:/ArataLeech74"
+    DESTINATION_FOLDER = "ArataLeech74" #Name of your folder read readme(not id of the folder)
+    RCLONE_CONFIG = """type = drive\nscope = drive\ntoken = {"access_token":"ya29.a0AfH6SMB7F7XWVgjthw4R9dUeeZOK63U-05Jz66jMF6SYYDGNFVibp9tolqkFL2MhFZ4TvM3keV4TDE638VAKbW5LBQTL_JoZ239SBh05Yq282BB2A4TVBpbhDq4oBOdXBLZMQ0FJO25d3KDd2HYEomO-hRNfp1cni3I9svbxis8","token_type":"Bearer","refresh_token":"1//0gRfZdrOUzoQLCgYIARAAGBASNwF-L9IrL_44xbl02Wh577bmsY7_gO4Ljb1EhEcF3comnA-hLdM6l-Iq7nOYfD2rPD2SaD53yZ8","expiry":"2020-11-27T03:42:39.8589741+08:00"}"""
+    UPLOAD_AS_DOC = "True"
+    #fill taking reference of this config, dont remove """ from both side of the RCLONE_CONFIG variable 
+    #FOR CUSTOM COMMANDS READ REAME AND FILL THEM...
